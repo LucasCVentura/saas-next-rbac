d1a1eee96149ef15463c1e1f9bc8ef059dda42cd
lucas coelho <lucascv8525@gmail.com>
2024-07-19 20:13:12 -0300

    createAccount, getProfile and authenticateWithPassword routes done

---
+++ apps/api/src/http/server.ts
@@ -0,0 +1,49 @@
+import { fastifyCors } from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import { fastifySwagger } from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+
+import { authenticateWithPassword } from './auth/authenticate-with-password'
+import { createAccount } from './auth/create-account'
+import { getProfile } from './auth/get-profile'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NextJS SaaS',
+      description: 'Full-stack app',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+app.register(fastifyCors)
+
+app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(getProfile)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('Server is running')
+})
