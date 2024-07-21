23f6354fb87a2b90f003c77762bd8831b107cd26
lucas coelho <lucascv8525@gmail.com>
2024-07-20 21:40:42 -0300

    get membership from a organization by organization slug

--- apps/api/@types/fastify.d.ts
+++ apps/api/@types/fastify.d.ts
@@ -1,7 +1,12 @@
 import 'fastify'
 
+import { Member, Organization } from '@prisma/client'
+
 declare module 'fastify' {
   export interface FastifyRequest {
     getCurrentUserId(): Promise<string>
+    getUserMembership(
+      slug: string,
+    ): Promisse<{ organization: Organization; membership: Member }>
   }
 }
